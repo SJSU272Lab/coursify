b3f9303582bc3cfdae81ba42de7b24a2737fca33
shafidayatar <shafi.dayatar@gmail.com>
2016-11-22 11:14:30 -0800

    Modified

--- src/rate_my_professor_api/rmy_get_feedback.js
+++ src/rate_my_professor_api/rmy_get_feedback.js
@@ -5,20 +5,7 @@ var callback = function(professor) {
     console.log("No professor found.");
     return;
   }
-  console.log("Name: " + professor.fname + " " + professor.lname);
-  console.log("University: "+ professor.university);
-  console.log("Quality: " + professor.quality);
-  console.log("Easiness: " + professor.easiness);
-  console.log("Helpfulness: " + professor.help);
-  console.log("Average Grade: " + professor.grade);
-  console.log("Chili: " + professor.chili);
-  console.log("URL: " + professor.url);
-  console.log("First comment: " + professor.comments[0]);
-  console.log("Clarity: " + professor.clarity);
-  console.log("Top Tags: " + professor.topTag);
-  console.log("Tags: " + professor.tags);
-  console.log("Courses : " + professor.courses);
-  console.log("CourseRatings: " + professor.courseRatings);
+    console.log(JSON.stringify(professor.data_load, null, 2));
 };
 var name = process.argv[2]; 
 rmp.get(name, callback);
