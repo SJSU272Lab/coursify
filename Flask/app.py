9c92294aab75f32096df99c27b7cb95ec9b983e4
Madhur Khandelwal <madhurkhandelwal234@gmail.com>
2016-11-22 16:53:19 -0800

    Serve all the web pages availabe via Flask
    TODO link all the pages together and parse form data

--- Flask/app.py
+++ Flask/app.py
@@ -7,7 +7,17 @@ app = flask.Flask("__main__")
 
 @app.route('/form1')
 def form1():
-    return "form1"
+    return flask.render_template('form1.html')
+
+
+@app.route('/form2')
+def form2():
+    return flask.render_template('form2.html')
+
+
+@app.route('/feedback')
+def feedback():
+    return flask.render_template('feedback.html')
 
 
 @app.route('/')
