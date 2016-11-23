ff1699ffeb15243f7b7726b31566a013857b19b2
Madhur Khandelwal <madhurkhandelwal234@gmail.com>
2016-11-22 18:31:29 -0800

    Fix form1 HTML pg & use session to store usr input

--- Flask/app.py
+++ Flask/app.py
@@ -1,8 +1,30 @@
 import flask
+import logging
 
-from flask import request
+from flask import request, session, url_for, redirect
 
 app = flask.Flask("__main__")
+# TODO Use a secure way to set this
+app.secret_key = 'aijdfAJKFJq234kjkdfsa]adfsdfasadfjjq4rjjJHJHjhakgj'
+
+
+@app.route('/formTest')
+def formTest():
+    return flask.render_template('formTest.html')
+
+
+@app.route('/form1validation', methods=['POST'])
+def form1validation():
+    # TODO don't trust user input, check if we can make this more secure
+    session['department'] = request.form['department']
+    session['major'] = request.form['major']
+    session['minor'] = request.form['minor']
+    session['gradYear'] = request.form['gradYear']
+    session['CulminatingExp'] = request.form['CulminatingExp']
+
+    for key in session:
+        logging.debug("%s: %s" % (key, session[key]))
+    return redirect(url_for('form2'))
 
 
 @app.route('/form1')
@@ -26,4 +48,5 @@ def landingPage():
 
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
     app.run(host="0.0.0.0", debug=True, port=5992)
