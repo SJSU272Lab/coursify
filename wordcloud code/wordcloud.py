9d96d6f16294ecbfad13e31eba9389be0b7cdca1
Chaya16 <chaya.malik89@gmail.com>
2016-11-26 12:12:06 -0800

    Wordcloud code

---
+++ wordcloud code/wordcloud.py
@@ -0,0 +1,28 @@
+from os import path
+from wordcloud import WordCloud
+import json
+
+
+# Read the whole text.
+
+f = open('samplejsonfile.json').read()
+
+data_dict = json.loads(f)
+
+
+tech_dict = data_dict["technology"]
+
+wordString = ""
+
+for items in tech_dict:
+	for count in range(items["count"]):
+            wordString += " " + (items["name"])
+
+ 
+# Generate a word cloud image
+
+wordcloud = WordCloud().generate(wordString)
+
+image = wordcloud.to_image()
+image.show()
+
