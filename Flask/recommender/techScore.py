67eda5ed59ab96c4f15b1798c16973cfee7e7213
Madhur Khandelwal <madhurkhandelwal234@gmail.com>
2016-11-28 23:55:52 -0800

    Implement techScore.py

---
+++ Flask/recommender/techScore.py
@@ -0,0 +1,67 @@
+# task: from a given review, for a list of keywords find the sentiment
+import json
+import requests
+import urllib
+
+with open("tech_list.json") as fp:
+    _techList = json.load(fp)
+
+
+def _getSentiment(line):
+    line = str(line)
+    # TODO move all the credentials and url info to config file
+    payload = {'outputMode': 'json',
+               'text': line,
+               "apikey": "e32e49b4c38bd84accea2cf1f72c32cf9e633c7f"}
+    payload = urllib.urlencode(payload)
+    payload.encode('ascii')
+    url = 'http://access.alchemyapi.com/calls/text/TextGetTextSentiment'
+    url = '%s?%s' % (url, payload)  # TODO there must be a better way ;P
+    r = requests.get(url)
+    docSentiment = json.loads(r.content)["docSentiment"]
+    if docSentiment["type"] == "neutral":
+        return None
+    else:
+        return float(docSentiment["score"])
+
+
+def getTechScore(review, techList=None):
+    # TODO use logging
+    # print "#" * 80
+    # print " " * 35 + "START"
+    # print "#" * 80
+    # print review
+    if techList is None:
+        techList = _techList
+    reviewLines = review.split(".")
+    reviewLines = [line.strip() for line in reviewLines]
+    reviewLines = [line.lower() for line in reviewLines]
+
+    techScoreList = {}
+    for tech in techList:
+        techScoreList[tech] = []
+
+    for line in reviewLines:
+        for tech in techList:
+            if tech in line:
+                sentiment = _getSentiment(line)
+                # print tech, line, sentiment
+                if sentiment:
+                    techScoreList[tech].append(sentiment)
+    # print techScoreList
+    techScore = {}
+    for tech in techScoreList:
+        if len(techScoreList[tech]):
+            techScore[tech] = sum(techScoreList[tech]) / len(
+                techScoreList[tech])
+    # print techScore
+    # print "#" * 80
+    # print " " * 35 + "END"
+    # print "#" * 80
+    return techScore
+
+
+if __name__ == "__main__":
+    review = """Prof taugh us docker very well. Flask was not good though.
+            He skipped Django. Flask was good then."""
+    print getTechScore(review)
